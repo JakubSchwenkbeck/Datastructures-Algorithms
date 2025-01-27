7f2690ed81edd574fc129a3563bb69b913b5a6c3
Jakub <jakub@schwenkbeck.de>
2025-01-27 23:55:01 +0100

    optimal stops and unit tests for trees

--- src/main/scala/algorithms/dynamic/optimalStops.scala
+++ src/main/scala/algorithms/dynamic/optimalStops.scala
@@ -1,43 +1,42 @@
 package algorithms.dynamic
 
-  case class Result(route: List[Int], minCost: Int)
+case class Result(route: List[Int], minCost: Int)
 
-  def findOptimalStops(a: Array[Int]): Result = {
-    val n = a.length
-    val dp = Array.fill(n)(Int.MaxValue) // Minimum penalty cost up to hotel i
-    val prev = Array.fill(n)(-1) // To reconstruct the route
+def findOptimalStops(a: Array[Int]): Result = {
+  val n = a.length
+  val dp = Array.fill(n)(Int.MaxValue) // Minimum penalty cost up to hotel i
+  val prev = Array.fill(n)(-1) // To reconstruct the route
 
-    dp(0) = 0 // No cost at the starting point
+  dp(0) = 0 // No cost at the starting point
 
-    // Dynamic Programming
-    for (j <- 1 until n) {
-      for (i <- 0 until j) {
-        val cost = Math.pow(200 - (a(j) - a(i)), 2).toInt
-        if (dp(i) + cost < dp(j)) {
-          dp(j) = dp(i) + cost
-          prev(j) = i
-        }
+  // Dynamic Programming
+  for (j <- 1 until n) {
+    for (i <- 0 until j) {
+      val cost = Math.pow(200 - (a(j) - a(i)), 2).toInt
+      if (dp(i) + cost < dp(j)) {
+        dp(j) = dp(i) + cost
+        prev(j) = i
       }
     }
-
-    // Route reconstruction
-    var route = List[Int]()
-    var current = n - 1
-    while (current != -1) {
-      route = current :: route // Add stop at the beginning of the list
-      current = prev(current)
-    }
-
-    Result(route, dp(n - 1))
   }
 
-  @main
-  def optimalStopsMain(): Unit = {
-    val hotels = Array(0, 100, 250, 400, 600, 800) // Hotel locations along the route
-
-    val result = findOptimalStops(hotels)
-
-    println(s"Optimal stops: ${result.route}")
-    println(s"Minimum penalty: ${result.minCost}")
+  // Route reconstruction
+  var route = List[Int]()
+  var current = n - 1
+  while (current != -1) {
+    route = current :: route // Add stop at the beginning of the list
+    current = prev(current)
   }
 
+  Result(route, dp(n - 1))
+}
+
+@main
+def optimalStopsMain(): Unit = {
+  val hotels = Array(0, 100, 250, 400, 600, 800) // Hotel locations along the route
+
+  val result = findOptimalStops(hotels)
+
+  println(s"Optimal stops: ${result.route}")
+  println(s"Minimum penalty: ${result.minCost}")
+}
---
+++ src/test/scala/datastructures/treeTest.scala
@@ -0,0 +1,91 @@
+package datastructures
+
+import munit.FunSuite
+
+class TreeTest extends FunSuite {
+
+  test("TreeNode: addChild should add a child node") {
+    val root = new TreeNode("Root")
+    val child = root.addChild("Child 1")
+
+    assertEquals(root.children.size, 1)
+    assertEquals(root.children.head.value, "Child 1")
+    assertEquals(child.parent.get.value, "Root")
+  }
+
+  test("TreeNode: removeChild should remove a child node by value") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = root.addChild("Child 2")
+
+    root.removeChild("Child 1")
+
+    assertEquals(root.children.size, 1)
+    assertEquals(root.children.head.value, "Child 2")
+  }
+
+  test("TreeNode: depth should return the correct depth of a node") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = child1.addChild("Child 2")
+
+    assertEquals(child2.depth, 2)
+    assertEquals(child1.depth, 1)
+    assertEquals(root.depth, 0)
+  }
+
+  test("TreeNode: height should return the correct height of a subtree") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = root.addChild("Child 2")
+    val grandchild = child1.addChild("Grandchild")
+
+    assertEquals(root.height, 2) // height of the subtree rooted at "Root"
+    assertEquals(child1.height, 1) // height of the subtree rooted at "Child 1"
+    assertEquals(child2.height, 0) // height of the subtree rooted at "Child 2"
+  }
+
+  test("TreeNode: find should return Some(node) if node exists") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = root.addChild("Child 2")
+
+    assertEquals(root.find("Child 1").map(_.value), Some("Child 1"))
+    assertEquals(root.find("Child 2").map(_.value), Some("Child 2"))
+    assertEquals(root.find("Nonexistent"), None)
+  }
+
+  test("TreeNode: printTree should print the tree structure") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = root.addChild("Child 2")
+    val grandchild = child1.addChild("Grandchild")
+
+    // This is a simple way to visually check if the output is correct
+    // By running this test, you'll verify the print format
+    root.printTree()
+    // Expected printed output:
+    // Root
+    //   Child 1
+    //     Grandchild
+    //   Child 2
+  }
+
+  test("TreeUtils: findRoot should return the root of the tree") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = child1.addChild("Child 2")
+
+    assertEquals(TreeUtils.findRoot(child2).value, "Root")
+  }
+
+  test("TreeUtils: countNodes should return the correct number of nodes") {
+    val root = new TreeNode("Root")
+    val child1 = root.addChild("Child 1")
+    val child2 = root.addChild("Child 2")
+    val grandchild = child1.addChild("Grandchild")
+
+    assertEquals(TreeUtils.countNodes(root), 4) // Root + 2 children + 1 grandchild
+  }
+
+}
