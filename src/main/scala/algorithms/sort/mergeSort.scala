1742e65297f1ca3833818fa17748971cd46b12a6
Jakub <jakub@schwenkbeck.de>
2025-01-27 14:58:44 +0100

    added generic mergeSort

--- src/main/scala/algorithms/sort/mergeSort.scala
+++ src/main/scala/algorithms/sort/mergeSort.scala
@@ -1,6 +1,11 @@
 package algorithms.sort
 
-///
+/// Merge Sort an immutable list of integers
+/// Example
+/// val Arr = [ 1 , 5 , 4 , 2 , 3 ]
+/// val result = mergeSort(Arr)
+/// println(result) // Returns [ 1 , 2 , 3 , 4 , 5 ]
+/// Selection sort runs in O(log n)
 def mergeSort(lst: List[Int]): List[Int] = {
   def merge(ls1: List[Int], ls2: List[Int]): List[Int] = {
     val k = ls2.length
@@ -19,6 +24,36 @@ def mergeSort(lst: List[Int]): List[Int] = {
   val (left, right) = lst.splitAt(lst.length / 2)
   if (lst.length > 1) {
     merge(mergeSort(left), mergeSort(right))
+  } else {
+    lst
+  }
+}
+
+/// Merge Sort an immutable list of integers
+/// generic with abstraction over type and comparison
+/// Example
+/// val Arr = [ 1 , 5 , 4 , 2 , 3 ]
+/// val result = mergeSort(Arr, (x : Int, y : Int) => x > y )
+/// println(result) // Returns [ 5 , 4 , 3 , 2 ,1]
+/// Selection sort runs in O(log n)
+def mergeSort[A](lst: List[A], cmp: (A, A) => Boolean): List[A] = {
+  def merge(ls1: List[A], ls2: List[A]): List[A] = {
+    val k = ls2.length
+    val l = ls1.length
+
+    if k == 0 then return ls1
+    if l == 0 then return ls2
+
+    if (cmp(ls1.head, ls2.head)) {
+      ls1.head :: merge(ls1.tail, ls2)
+    } else {
+      ls2.head :: merge(ls1, ls2.tail)
+    }
+  }
+
+  val (left, right) = lst.splitAt(lst.length / 2)
+  if (lst.length > 1) {
+    merge(mergeSort(left, cmp), mergeSort(right, cmp))
   } else {
     lst
   }
--- src/test/scala/algorithms/sort/mergeSort.scala
+++ src/test/scala/algorithms/sort/mergeSort.scala
@@ -1,7 +1,6 @@
 package algorithms.sort
 
 import algorithms.sort.mergeSort
-
 import munit.*
 
 class MergeSortTest extends FunSuite {
@@ -32,6 +31,38 @@ class MergeSortTest extends FunSuite {
   test("merge-sort (not in-place) handles empty arrays") {
     val arr = List.empty[Int]
     val sortedArr = mergeSort(arr)
+    assert(sortedArr.isEmpty)
+  }
+}
+
+class GenericMergeSortTest extends FunSuite {
+  test("generic-merge-sort (not in-place) sorts arrays correctly") {
+    val arr = List(4, 2, 1, 5, 6, 3, 7, 12, 8, 10, 9, 11)
+    val sortedArr = mergeSort(arr, (x: Int, y: Int) => x >= y)
+    assertEquals(sortedArr, List(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
+  }
+
+  test("generic-merge-sort (not in-place) sorts arrays with negatives correctly") {
+    val arr = List(-1, 4, 2, 1, -4, 5, 6, 3, 0, 7, 12, -3, 8, 10, 9, -2, 11)
+    val sortedArr = mergeSort(arr, (x: Int, y: Int) => x >= y)
+    assertEquals(sortedArr, List(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4))
+  }
+
+  test("generic-merge-sort (not in-place) sorts arrays with duplicates") {
+    val arr = List(4, 2, 1, 5, 3, 2)
+    val sortedArr = mergeSort(arr, (x: Int, y: Int) => x >= y)
+    assertEquals(sortedArr, List(5, 4, 3, 2, 1))
+  }
+
+  test("generic-merge-sort (not in-place) handles singleton arrays") {
+    val arr = List(1)
+    val sortedArr = mergeSort(arr, (x: Int, y: Int) => x >= y)
+    assertEquals(sortedArr, List(1))
+  }
+
+  test("generic-merge-sort (not in-place) handles empty arrays") {
+    val arr = List.empty[Int]
+    val sortedArr = mergeSort(arr, (x: Int, y: Int) => x >= y)
     assert(sortedArr.isEmpty)
   }
 }
