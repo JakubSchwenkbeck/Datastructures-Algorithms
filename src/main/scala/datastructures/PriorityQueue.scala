43265144a2d41edfdfb9e7c6fe9472031387f136
Jakub <jakub@schwenkbeck.de>
2025-01-28 12:12:24 +0100

    added PriorityQueue via MaxHeap

---
+++ src/main/scala/datastructures/PriorityQueue.scala
@@ -0,0 +1,31 @@
+package datastructures
+
+import datastructures.heap.MaxHeap
+
+class PriorityQueue[T](implicit ord: Ordering[T]) {
+  private val maxHeap = new MaxHeap[T]()
+
+  // Insert an element into the priority queue
+  def enqueue(value: T): Unit = {
+    maxHeap.insert(value)
+  }
+
+  // Remove and return the highest-priority element
+  def dequeue(): Option[T] = {
+    maxHeap.extractMax()
+  }
+
+  // Peek at the highest-priority element without removing it
+  def peek(): Option[T] = {
+    maxHeap.peekMax()
+  }
+
+  // Check if the queue is empty
+  def isEmpty: Boolean = maxHeap.isEmpty
+
+  // Get the number of elements in the queue
+  def size: Int = maxHeap.size
+
+  // Debugging utility: Print the queue elements
+  def printQueue(): Unit = maxHeap.printHeap()
+}
---
+++ src/test/scala/datastructures/PriorityQueueTest.scala
@@ -0,0 +1,67 @@
+package datastructures
+
+import munit._
+import datastructures.PriorityQueue
+
+class PriorityQueueTest extends FunSuite {
+
+  test("PriorityQueue[Int]: enqueue and dequeue") {
+    val pq = new PriorityQueue[Int]()
+    pq.enqueue(10)
+    pq.enqueue(30)
+    pq.enqueue(20)
+
+    assertEquals(pq.dequeue(), Some(30))
+    assertEquals(pq.dequeue(), Some(20))
+    assertEquals(pq.dequeue(), Some(10))
+  }
+
+  test("PriorityQueue[Int]: peek should return the highest-priority item without removing it") {
+    val pq = new PriorityQueue[Int]()
+    pq.enqueue(10)
+    pq.enqueue(50)
+    pq.enqueue(20)
+
+    assertEquals(pq.peek(), Some(50))
+    assertEquals(pq.dequeue(), Some(50))
+    assertEquals(pq.peek(), Some(20))
+  }
+
+  test("PriorityQueue[Int]: isEmpty should return true for an empty queue") {
+    val pq = new PriorityQueue[Int]()
+    assert(pq.isEmpty)
+  }
+
+  test("PriorityQueue[Int]: isEmpty should return false after enqueueing items") {
+    val pq = new PriorityQueue[Int]()
+    pq.enqueue(5)
+    assert(!pq.isEmpty)
+  }
+
+  test("PriorityQueue[Int]: dequeue from an empty queue should return None") {
+    val pq = new PriorityQueue[Int]()
+    assertEquals(pq.dequeue(), None)
+  }
+
+  test("PriorityQueue[Int]: enqueue and dequeue with different types of data") {
+    val pq = new PriorityQueue[String]()
+    pq.enqueue("apple")
+    pq.enqueue("banana")
+    pq.enqueue("cherry")
+
+    assertEquals(pq.dequeue(), Some("cherry")) // Lexicographical order (max priority)
+    assertEquals(pq.dequeue(), Some("banana"))
+    assertEquals(pq.dequeue(), Some("apple"))
+  }
+
+  test("PriorityQueue[Int]: size should return the correct number of elements") {
+    val pq = new PriorityQueue[Int]()
+    pq.enqueue(10)
+    pq.enqueue(20)
+    pq.enqueue(30)
+
+    assertEquals(pq.size, 3)
+    pq.dequeue()
+    assertEquals(pq.size, 2)
+  }
+}
